f27c13d991d86f8b39b3a5db664b009d84baae25
azimezn <117550865+azimezn@users.noreply.github.com>
2022-12-08 11:58:24 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # work-day-scheduler
+
+## Description
+
+Type in your schedule and save so you can access the same information later.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+You can reach this webpage through any web browser. No installation is required.
+https://azimezn.github.io/work-day-scheduler/
+
+## Usage
+
+Type in any time block and click the save button to save your schedule so you can access the information again. The times are color coded to show which times are in the past, present, or future.
+
+![screenshot of coding quiz main page](assets/images/screenshot.png)
+
+## Credits
+
+- Rutgers University Coding Bootcamp
+- [w3schools.com](W3schools.com)
+
+## License
+
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,16 +11,16 @@ console.log(todayTime);
 
 var hourText;
 
-function init () {
+function init() {
   // display current date
   $("#currentDay").text("Today is " + todayDate);
 
   // retrieve events that were written in the schedule from the local storage
-  timeBlock.each(function(i){
+  timeBlock.each(function (i) {
     $(this).children("textarea").val(localStorage.getItem($(this).attr("id")));
   })
 
-  timeBlock.each(function(i){
+  timeBlock.each(function (i) {
     // split the id to get the hour number to compare with the current hour
     hourText = parseInt($(this).attr("id").split("-")[1]);
     console.log(i, hourText);
@@ -29,15 +29,16 @@ function init () {
     if (todayTime < hourText) {
       $(this).removeClass("present").addClass("future");
     }
-      else if (todayTime > hourText) {
-        $(this).removeClass("present").addClass("past");
-        return;
-      }
+    else if (todayTime > hourText) {
+      $(this).removeClass("present").addClass("past");
+      return;
     }
-)}
+  }
+  )
+}
 
 saveButton.on('click', function () {
-  timeBlock.each(function(i){
+  timeBlock.each(function (i) {
     localStorage.setItem($(this).attr("id"), $(this).children("textarea").val());
   })
 });
